5c7d2dac614cdee12cdbb4d9f27586c4c45cfddc
Ferdiansyah <30854454+frdiansyah11@users.noreply.github.com>
2017-10-11 01:24:10 +0700

    Update PlatformComponent.java

--- core/src/runpassworld/Model/PlatformComponent.java
+++ core/src/runpassworld/Model/PlatformComponent.java
@@ -8,8 +8,6 @@ import com.badlogic.gdx.math.Vector2;
  * Created by Nicoy on 10/10/2017.
  */
 
-    //class untuk membuat platform semisalkan bergerak vertikal
-
 public class PlatformComponent implements Component {
     public static Vector2 originalPosition;
     public static float timePassed = 0;
